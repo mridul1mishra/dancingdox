75e75a7411789001d16aa3453dc7c13eaf7c4303
Divyesh Mishra <mridul1mishra@yahoo.com>
2025-07-30 19:07:49 -0400

    message

--- backend/utils/sql.js
+++ backend/utils/sql.js
@@ -1,11 +1,3 @@
 const mysql = require('mysql2/promise');
 
-const pool = mysql.createPool({
-  host: 'localhost',
-  user: 'root',
-  password: '98#Mridul',
-  database: 'dashdoxs',
-  port: 3306,
-});
-
 module.exports = pool;
